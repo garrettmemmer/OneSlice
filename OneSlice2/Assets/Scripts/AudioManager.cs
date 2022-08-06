da6c0202086f57e9cd0c0b127745d9b062fac609
Garrett Memmer <garrett3711@att.net>
2022-08-05 21:44:48 -0500

    working on adding sounds

---
+++ OneSlice2/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+             s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
