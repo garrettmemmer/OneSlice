36360a480fbcc95caa58ea8ee2f0d8cdc3776a37
Garrett Memmer <garrett3711@att.net>
2022-12-17 11:28:17 -0700

    oneSlice misc

--- OneSlice2/Assets/Scripts/GameController.cs
+++ OneSlice2/Assets/Scripts/GameController.cs
@@ -7,25 +7,38 @@ using TMPro;
 public class GameController : MonoBehaviour
 {
     public static GameController instance;
-    public GameObject player1;
-    public GameObject cpu1;
-    public GameObject cpu2;
-    public GameObject cpu3;
-    public GameObject cpu4;
-    public GameObject cpu5;
-    public GameObject cpu6; 
+    public GameObject[] Enemies;
+    public GameObject[] EnemiesIdle;
+    public GameObject[] EnemyPool;
+    public GameObject[] EnemyIdlePool;
+    // public GameObject cpu1;
+    // public GameObject cpu2;
+    // public GameObject cpu3;
+    // public GameObject cpu4;
+    // public GameObject cpu5;
+    // public GameObject cpu6; 
+    public Renderer enemy;
+    public Material[] materials;
+
     public GameObject enemyFightingSpot;
 
+
     public GameController gc;
-    //public CameraShake cameraShake; 
+    public CameraShake cameraShake; 
     public TraumaInducer tramaInducer;
     public State currentState;
 
     public int currentRound = 1;
+    public int currentFloor = 1;
 
     public TextMeshProUGUI txt;
+    public TextMeshProUGUI floorCounterTxt;
+    public Canvas newDecisionCanvas;
+    public Animator CanvasAnimatior;
     public Canvas readyCanvas;
+    public Animator ReactionCanvasAnimatior;
     public Canvas reactionCanvas;
+    public Image reactionImage;
     public Canvas decisionCanvas;
     public Canvas nextRoundCanvas;
     public Canvas transitionCanvas;
@@ -66,25 +79,35 @@ public class GameController : MonoBehaviour
         STATE_FALSEALARM,    //3.2
         STATE_ATTACK,  //4
         STATE_DECISION, //5
-        STATE_RESET    //6
+        STATE_RESET,    //6
+        STATE_NEWROOM
     }
 
     void Start()
     {
         defaultTransitionScale = transitionCanvas.transform.localScale;
         defaultTransitionPosition = transitionCanvas.transform.position;
-        reactionCanvas.enabled = false;
+        //reactionCanvas.enabled = false;
+        //reactionImage.enabled = false;
         decisionCanvas.enabled = false;
         nextRoundCanvas.enabled = false;
         transitionCanvas.enabled = false;
         scaleChange = new Vector3(0f, -0.005f, 0f);
-        positionChange = new Vector3(2f, 0f, 0.0f);
+        positionChange = new Vector3(3f, 0f, 0.0f);
         currentState = State.STATE_IDLE;
+
+        //materialTest = GetComponent<Renderer>().material;
     }
 
     void Update()
     {
         UpdateState();
+
+        //if (Input.GetKeyDown("space"))
+        //{
+            //enemy.material = materials[1];
+            // Enemies.GetComponent<MeshRenderer>().material = Material1; potential way for procedural colors
+        //}
     }
 
     void UpdateState()
@@ -99,13 +122,17 @@ public class GameController : MonoBehaviour
                 break;
 
             case State.STATE_STANCE: //2
+                CanvasAnimatior.SetInteger("transitionNumber", 0);
                 animationState = 1;
                 animationStateCPU = 1;
                 StanceLogic();
                 break;
 
             case State.STATE_GO: //3
-                reactionCanvas.enabled = true;
+                //reactionCanvas.enabled = true;
+                ReactionCanvasAnimatior.SetInteger("transitionNumber", 0);
+                //reactionImage.enabled = false;
+                decisionCanvas.enabled = true;
                 GoLogic();
                 break;
 
@@ -117,10 +144,14 @@ public class GameController : MonoBehaviour
                 break;
 
             case State.STATE_ATTACK: //4
-                reactionCanvas.enabled = false;
+               // reactionCanvas.enabled = false;
+                //reactionImage.enabled = false;
+                ReactionCanvasAnimatior.SetInteger("transitionNumber", 1);
                 decisionCanvas.enabled = true;
                 //adding screenshake here
-                // StartCoroutine(cameraShake.Shake(.15f, .4f)); this was clunckuy with my own code
+                StartCoroutine(cameraShake.Shake(.001f, .2f)); 
+                //this was clunckuy with my own code
+                
                 //lets try with the assest store :)
                 StartCoroutine(tramaInducer.StartShaking());
                 //transitionCanvas.enabled = true;
@@ -129,6 +160,7 @@ public class GameController : MonoBehaviour
 
             case State.STATE_DECISION: //5
                 playerInput = false;
+                CanvasAnimatior.SetInteger("transitionNumber", 0);
                 DecisionLogic();
                 break;
 
@@ -140,6 +172,21 @@ public class GameController : MonoBehaviour
                 timerValue = 0.0f;
                 txt.text = "0.0";//timerValue.ToString();
                 LoadRound(currentRound);
+                break;
+
+            case State.STATE_NEWROOM: //7
+                //new room transition
+                CanvasAnimatior.SetInteger("transitionNumber", 0);
+
+                //new room sound
+
+                //deactivate old enemy
+
+                //spawn 2 new watchers
+
+                //spawn new fighting enemy
+
+                //set state to idle?
                 break;
 
             default:
@@ -202,11 +249,11 @@ public class GameController : MonoBehaviour
 
     public void AttackLogic()
     {
-        //TransitionScreen();
-        //play the slice sound here
-        //FindObjectOfType<AudioManager>().Play("SwordSlice");
+
         FindObjectOfType<AudioManager>().Play("Slice");
-        SliceScreen(); // i think that slice screen needs to be the one that makes the transition to a new state
+
+        CanvasAnimatior.SetInteger("transitionNumber", 1); 
+
         if (playerWin)
         {
             animationState = 2;
@@ -217,11 +264,12 @@ public class GameController : MonoBehaviour
             //FindObjectOfType<AudioManager>().Play("SheathSword");
             //FindObjectOfType<AudioManager>().Play("EnemyDeath");
 
-            StartCoroutine(waiter(4));
+            StartCoroutine(waiter(2));
             if (waiterBool)
             {
                 waiterBool = false;
-               // currentState = State.STATE_DECISION;
+                currentState = State.STATE_DECISION;
+                //waiterBool = false;
             }
         }
         if (cpuWin)
@@ -252,11 +300,21 @@ public class GameController : MonoBehaviour
 
         if(playerInput)
         {
-           print("next round would start here");
-           if(currentRound != 4)
-           {
-                currentRound += 1;
+            currentRound += 1;
+            currentState = State.STATE_RESET;
+
+
+            print("next round would start here");
+            //last round logic
+            if(currentRound > 3) // last round
+            {
+                currentRound = 0;
+                currentFloor += 1;
+                string currentFloorTxtHolder = "FLOOR: ";
+               // floorCounterTxt.txt = (currentFloorTxtHolder + currentFloor).ToString;
+                floorCounterTxt.SetText("Floor: " + currentFloor);//) = ("Floor: " + currentFloor).ToString;
                 currentState = State.STATE_RESET;
+                //currentState = State.STATE_NEWFLOOR;
             } 
         }
     }
@@ -341,51 +399,125 @@ public class GameController : MonoBehaviour
     public void LoadRound(int n)
     {
         //page turn animation
-        transitionCanvas.enabled = true;
+        //transitionCanvas.enabled = true;
         waiter(1);
         if (waiterBool)
         {
             TransitionScreen();
+            //HERE to play UI animation
+            CanvasAnimatior.SetInteger("transitionNumber", 2);
+            //waiter(2);
+            //currentState = State.STATE_IDLE;
         }
         //need to reset both transition screens
         //switch to enemies[n]
+
         SwitchEnemies();
+        //CanvasAnimatior.SetInteger("transitionNumber", 0);
     }
 
     public void SwitchEnemies()
     {
         if(currentRound == 1)
         {
-            //idek if anything will be done here
+            //idek if anything will be done here  
         }
         if (currentRound == 2)
         {
-            //set cpu2 to main ene    my  
-            //set cpu1 position
-            //set cpu to onlooker
-            cpu1.SetActive(false);
-            cpu2.transform.position = enemyFightingSpot.transform.position;
+            Enemies[0].SetActive(false);
+            Enemies[1].SetActive(true);
+            EnemiesIdle[0].SetActive(false);
+            //EnemiesIdle[0].transform.position = enemyFightingSpot.transform.position;
+
+            //cpu1.SetActive(false);
+            //cpu2.transform.position = enemyFightingSpot.transform.position;
         }
         if (currentRound == 3)
         {
-            cpu2.SetActive(false);
-            cpu3.transform.position = enemyFightingSpot.transform.position;
+            Enemies[1].SetActive(false);
+            Enemies[2].SetActive(true);
+            EnemiesIdle[1].SetActive(false);
+            //EnemiesIdle[1].transform.position = enemyFightingSpot.transform.position;
+            //cpu2.SetActive(false);
+            //cpu3.transform.position = enemyFightingSpot.transform.position;
         }
         if (currentRound == 4)
         {
-            cpu3.SetActive(false);
-            cpu4.transform.position = enemyFightingSpot.transform.position;
+            Enemies[2].SetActive(false);
+            Enemies[3].SetActive(true);
+            EnemiesIdle[2].SetActive(false);
+            //EnemiesIdle[2].transform.position = enemyFightingSpot.transform.position;
+            //cpu3.SetActive(false);
+            //cpu4.transform.position = enemyFightingSpot.transform.position;
         }
         if (currentRound == 5)
         {
-            cpu4.SetActive(false);
-            cpu5.transform.position = enemyFightingSpot.transform.position;
+            Enemies[3].SetActive(false);
+            Enemies[4].SetActive(true);
+            EnemiesIdle[3].SetActive(false);
+            //EnemiesIdle[3].transform.position = enemyFightingSpot.transform.position;
+            //cpu4.SetActive(false);
+            //cpu5.transform.position = enemyFightingSpot.transform.position;
         }
         if (currentRound == 6)
         {
-            cpu5.SetActive(false);
-            cpu6.transform.position = enemyFightingSpot.transform.position;
+            Enemies[4].SetActive(false);
+            Enemies[5].SetActive(true);
+            EnemiesIdle[4].SetActive(false);
+            //EnemiesIdle[4].transform.position = enemyFightingSpot.transform.position;
+            //cpu5.SetActive(false);
+            //cpu6.transform.position = enemyFightingSpot.transform.position;
         }
+    }
+
+
+
+    public void GenerateEnemies()
+    {
+        if (currentRound == 1)
+        {
+            Enemies[0].SetActive(true);
+            EnemiesIdle[0].SetActive(true);
+            EnemiesIdle[1].SetActive(true);
+
+            //var enemy1 = Enemies[0]; V 
+            //var enemy1Skin = enemy1.GetChild(0);
+            //var enemy1Joints = enemy1.GetChild(1);
+            Enemies[0].GetComponent<MeshRenderer>().material = materials[1];
+
+            //Enemies[0].GetComponentInChild<MeshRenderer>().material = materials[1]; //potential way for procedural colors
+            //EnemiesIdle[0].GetComponent<MeshRenderer>().material = materials[1];
+        }
+        if (currentRound == 2)
+        {
+            Enemies[0].SetActive(false);
+            Enemies[1].SetActive(true);
+            EnemiesIdle[0].SetActive(false);
+        }
+        if (currentRound == 3)
+        {
+            Enemies[1].SetActive(false);
+            Enemies[2].SetActive(true);
+            EnemiesIdle[1].SetActive(false);
+        }
+
+
+
+
+        //spawn 2 new watchers
+
+        //spawn new fighting enemy
+
+    }
+
+    public void GenerateProceduralEnemies()
+    {
+
+        //spawn 2 new watchers
+
+        //spawn new fighting enemy
+
+        //generate colors for new enemies 
     }
 }
 
